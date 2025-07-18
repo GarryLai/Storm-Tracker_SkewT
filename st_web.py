5412555b8248080fd3f15b77deb8c3d5cdd7c752
Garry Lai <garrylai0306@gmail.com>
2025-07-19 03:42:52 +0800

    Update st_web.py

--- st_web.py
+++ st_web.py
@@ -22,7 +22,7 @@ st.title('Storm Tracker Skew-T')
 pd.set_option('display.max_columns', None)
 
 def opposite_az(az):
-	if az.item() > 180:
+	if az.any() > 180:
 		return az - 180
 	else:
 		return az + 180
